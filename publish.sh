985e42a2658256dd919c415cd48838cfcb43491d
Yoshihide Chubachi <yoshi@chubachi.net>
2011-12-05 15:11:23 +0900

    Prepared for publishing

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 git checkout master
 git add .
-git commit -a -m 'Published'
+git commit -a -m 'Prepared for publishing'
+# Publish
 git checkout gh-pages
 git checkout master build
+mv build/html/* .
+rm -rf build/html
 git add .
-git commit -a -m 'Published ph-pages'
+git commit -a -m 'Published'
 git push
 git checkout master
